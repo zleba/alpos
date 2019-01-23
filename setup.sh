0f5bb06529d4a1c41c26c8aebf05f5ef2ce416db
Radek Zlebcik <radek.zlebcik@cern.ch>
2019-01-23 19:06:12 +0100

    Adding CI

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
+if [ -d "/cvmfs/sft.cern.ch/lcg" ]; then
 source /cvmfs/sft.cern.ch/lcg/views/LCG_94/x86_64-slc6-gcc7-opt/setup.sh
 export LHAPDF_DATA_PATH=/cvmfs/sft.cern.ch/lcg/external/lhapdfsets/current/:`lhapdf-config --datadir`
+else
+echo "The cvmfs file system not mounted"
+fi
 
 export PATH=$PWD/qcdnumBuild/bin:$PATH
 export PATH=$PWD/fastnloBuild/bin:$PATH
