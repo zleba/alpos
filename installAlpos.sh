ec057dff4bc182df8e5ec906eea63ad406be71ce
Radek Zlebcik <zlebcr@mail.desy.de>
2019-01-26 00:46:54 +0100

    cmake version recognition

--- installAlpos.sh
+++ installAlpos.sh
@@ -5,7 +5,14 @@ FNLODIR=$pwd/fastnloBuild
 if [[ "$USER" = dnb ]]; then
     FNLODIR=/afs/ipp-garching.mpg.de/home/d/dnb/ATLAS/fastNLO/fastNLO/install
 fi
-cmake3 ../alpos -DFNLO_PREFIX=$FNLODIR -DQCDNUM_PREFIX=$pwd/qcdnumBuild -DAPFELxx_PREFIX=$pwd/apfelxx -DCMAKE_INSTALL_PREFIX=$pwd/alposBuild
+
+cmakeCMD=cmake
+if [ `command -v cmake3`  ]; then
+    cmakeCMD=cmake3
+fi
+echo using $cmakeCMD
+
+$cmakeCMD  ../alpos -DFNLO_PREFIX=$FNLODIR -DQCDNUM_PREFIX=$pwd/qcdnumBuild -DAPFELxx_PREFIX=$pwd/apfelxx -DCMAKE_INSTALL_PREFIX=$pwd/alposBuild
 
 make -j`nproc`
 #make install
