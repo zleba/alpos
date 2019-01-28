5abb94120c91b023f099cd9774f3364922bb9965
Daniel Britzger <dnb@mppui4.t2.rzg.mpg.de>
2019-01-28 16:42:44 +0100

    DPDF function

--- alpos/src/alpos/functions/ADPDF.cc
+++ alpos/src/alpos/functions/ADPDF.cc
@@ -19,9 +19,13 @@ const std::vector<std::string> ADPDF::fRequirements = {
    "pom1", // pomeron 1, this must be a 'PDF'
    "pom2", // pomeron 2, this must be a 'PDF'
    "reg1", // reggeon 1, this must be a 'PDF'
+   "xPomFluxNorm", "tcut" ,// flux
+   "Flux_pom1_a0","Flux_pom1_ap","Flux_pom1_b0",
+   "Flux_pom2_a0","Flux_pom2_ap","Flux_pom2_b0",
+   "Flux_reg1_a0","Flux_reg1_ap","Flux_reg1_b0","reg1_n",
 }; //< List of all AParm's which this function depends on
 const std::vector<std::string> ADPDF::fStopFurtherNotification = {"xpom","zpom","muf"}; //< List of Parm's which have changed, but this function does not notify further dependencies
-const std::string ADPDF::fFunctionName = "ApfelxxPDF"; //< The function's name
+const std::string ADPDF::fFunctionName = "DPDF"; //< The function's name
 
 
 // __________________________________________________________________________________________ //
@@ -64,7 +68,6 @@ std::vector<double> ADPDF::GetQuick(int n, ...) {
    double zpom  = va_arg(ap, double);
    double muf = va_arg(ap, double);
    va_end(ap);
-
    return GetQuick({xpom,zpom,muf});
     
 }
@@ -80,21 +83,58 @@ std::vector<double> ADPDF::GetQuick(const vector<double>& xpom_zpom_muf) {
    //!   xp_mur[0] = xp
    //!   xp_muf[0] = Q
 
-
    std::vector<double> pdf(fValue.size(),0);
    if ( xpom_zpom_muf.size() != 3) {
       error["GetQuick"]<<"Quick acces is implemented for two parameter which are 'xpom','zpom' and 'muf'."<<endl;
       return pdf;
    }
+   double xpom = xpom_zpom_muf[0];
+   double zpom = xpom_zpom_muf[1];
+   double muf  = xpom_zpom_muf[2];
 
-   // if calculation failed, return 'fudged' values
-   if ( fValue[6]==1 ) { // PDF is nan (see below)
-      vector<double> ret{0, 0.1, 0.1, 0.1, 0.1, 0.1, 1, 0.1, 0.1, 0.1, 0.1, 0.1, 0};
-      return ret;
-   }    
+   // flux parameters
+   double tcut = PAR(tcut);
+   double xPomNorm= PAR(xPomFluxNorm);
 
-   cout<<"Daniel ADPDF::GetQuick2 Todo!"<<endl;
-   // quick (pdf1, pdf2, reg1)
+   // pomeron
+   pdf = QUICK(pom1,({zpom,muf}));
+   //Pomeron flux
+   double a0_IP = PAR(Flux_pom1_a0);
+   double ap_IP = PAR(Flux_pom1_ap);
+   double b0_IP = PAR(Flux_pom1_b0);
+   double flxIP = rflux(xpom, tcut, a0_IP, ap_IP, b0_IP, xPomNorm);
+   for ( auto& p : pdf ) p*= flxIP;
+
+
+   // pom2
+   double a0_P2 = PAR(Flux_pom2_a0);
+   if ( a0_P2!=0 ) {
+      vector<double> pom2 = QUICK(pom2,({zpom,muf}));
+      if ( pom2.size() != 13 ) {
+	 error["Quick"]<<"pom2 is requested (Flux_pom2_a0!=0), but no reasonable PDF function is provided."<<endl;
+	 exit(1);
+      }
+      double ap_P2 = PAR(Flux_pom2_ap);
+      double b0_P2 = PAR(Flux_pom2_b0);
+      double flxP2 = rflux(xpom, tcut, a0_P2, ap_P2, b0_P2, xPomNorm);
+      for ( int i = 0 ; i < 13 ; i++ ) 
+	 pdf[i] += pom2[i] * flxP2;
+   }
+
+   // reggeon
+   vector<double> reg1 = QUICK(reg1,({zpom,muf}));
+   //Reggeon flux 
+   double n_IR = PAR(reg1_n);
+   if ( n_IR!=0 ) {
+      double a0_IR = PAR(Flux_reg1_a0);
+      double ap_IR = PAR(Flux_reg1_ap);
+      double b0_IR = PAR(Flux_reg1_b0);
+      double flxIR = rflux(xpom, tcut, a0_IR, ap_IR, b0_IR, xPomNorm);
+      if ( reg1.size()==13) {
+	 for ( int i = 0 ; i < 13 ; i++ ) 
+	    pdf[i] += reg1[i] * flxIR * n_IR;
+      }
+   }
    
    return pdf;
   
@@ -109,17 +149,59 @@ bool ADPDF::Update() {
 
    cout<<"Daniel ADPDF::Update Todo!"<<endl;
 
+   // update
+   SET(pom1.xp,PAR(xpom),0);
+   SET(pom1.muf,PAR(muf),0);
+   if ( PAR(Flux_pom2_a0) ) {
+      SET(pom2.xp,PAR(xpom),0);
+      SET(pom2.muf,PAR(muf),0);
+   }
+   if ( PAR(reg1_n) != 0 ) {
+      SET(reg1.xp,PAR(xpom),0);
+      SET(reg1.muf,PAR(muf),0);
+   }
+
+
    vector<double> pom1 = VALUES(pom1);
    vector<double> pom2 = VALUES(pom1);
    vector<double> reg1 = VALUES(reg1);
 
-   if ( pom2.size() == 1 && pom2[0]==0 ) {
-      cout<<"test: no pom2 specified."<<endl;
-   }
-
-   fValue = pom1;
-   
+   fValue = GetQuick(vector<double>{PAR(xpom),PAR(zpom),PAR(muf)});
 
    return true;
 }
+
+//tcut is negative : tcut = -1
+double ADPDF::rfluxRaw(double xpom, double a0, double ap, double b0, double tcut)
+{
+   const double mp = 0.93827231;
+
+   //     calc min. kinematically  allowed t
+   double tmin= -pow(mp*xpom,2)/(1.-xpom);
+
+   //     c*xpom**(-(2apom-1))
+   double fl =  exp((2.0*a0-1.)*log(1.0/xpom));
+   double b=(b0+2.0*ap*log(1.0/xpom));
+
+   //   at fixed t:  exp(Bt)
+   //  fl = fl * exp(b*tcut);
+
+   //   t-integrated: (1/B)*[exp(-B*tmax)-exp(-B*tmin)]
+   fl = fl * (exp(tmin*b)-exp(tcut*b))/b;
+
+   return fl;
+}
+
+
+double ADPDF::rflux(double xpom, double tcut, double a0, double ap, double b0, double xPomNorm)
+{
+   // double tcut = -1;
+   // double xPomNorm = 0.003;
+   const double dm =  rfluxRaw(xPomNorm, a0, ap, b0, tcut);
+   double  norm=(1./(xPomNorm*dm)); //xpom * flux normalized to 1 at xpom = 0.003
+
+   return  norm * rfluxRaw(xpom, a0, ap, b0, tcut);
+}
+
+
 
