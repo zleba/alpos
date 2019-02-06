5224bfe9e139cefd917886a762ee5723efa249b8
Radek Zlebcik <zlebcr@mail.desy.de>
2019-02-06 12:29:24 +0100

    header

--- alpos/include/alpos/functions/ADPDF.h
+++ alpos/include/alpos/functions/ADPDF.h
@@ -25,13 +25,13 @@ public:
    virtual std::vector<double> GetQuick(int n,...); //< The possibilty to implement a quick access without changing of any parameters
    virtual std::vector<double> GetQuick(const std::vector<double>&); //< Another possibilty to implement a quick access without changing of any parameters or recent (member-)values
 
-   double rfluxRaw(double x_pom, double a0, double ap, double b0, double tcut);
+   //double rfluxRaw(double x_pom, double a0, double ap, double b0, double tcut);
    double rflux(double xpom, double tcut, double a0, double ap, double b0, double xPomNorm);
    
 private:
    static const std::vector<std::string> fRequirements; //< List of all AParm's which this function depends on
    static const std::vector<std::string> fStopFurtherNotification; //< List of Parm's which have changed, but this function does not notify further dependencies
-
+   double fPomNorm, fRegNorm;
 
 protected:
 };
