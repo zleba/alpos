a09fa078b9a7b536a5cbc3c36520f5a4c9884041
Radek Zlebcik <zlebcr@mail.desy.de>
2019-02-18 16:56:59 +0100

    Allowing larger grids in Apfel

--- installApfel.sh
+++ installApfel.sh
@@ -1,9 +1,12 @@
 pwd=$PWD
 mkdir -p apfel
 cd apfel
-git clone  https://github.com/scarrazza/apfel.git
+git clone  --depth 1  https://github.com/scarrazza/apfel.git
 cd apfel
 echo $PWD
+
+sed -i 's/parameter(nint_max=200)/parameter(nint_max=301)/;s/parameter(nint_max_DIS=120)/parameter(nint_max_DIS=220)/'   src/commons/grid.h
+
 #echo "set_property(TARGET dis PROPERTY POSITION_INDEPENDENT_CODE ON) " >> apfelxx/src/dis/CMakeLists.txt
 #cat apfelxx/src/dis/CMakeLists.txt
 ./configure --prefix=$pwd/apfel/ 
